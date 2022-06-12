6f543c615793f21b2ff3c2a14f7be2a9ddf98d27
jeanian26 <paulian829@gmail.com>
2022-06-12 13:31:13 +0800

    Updates

--- firebase.py
+++ firebase.py
@@ -3,19 +3,20 @@ from firebase_admin import credentials, auth,db
 
 cred = credentials.Certificate({
   "type": "service_account",
-  "project_id": "ramen-nado-86f76",
-  "private_key_id": "bc33a83401cbbaba64f9d0b3eebe9855ee54d617",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCedoQ6GlwBdc/A\nDGZ+wCJP9BOIlsWTrcOSc3A5polgKrDykzGL9QgF7WDh9SduNfkMCJ5MSH795MWd\nbhXD5d7z6V7AbKGuYbeGxUp1NDtzSdpjmd9IpAMhlpJVijVwUno9zDMfaNKxoWQh\nLByLbe0mK/iS1KEsPPCwJi0+CAb+f7wKkdktoSFks7MufvVcIMDOZJxpyfGm6sO2\nXY9FjlujvstuHd2nobd5WWl9bMYUT5dtq3GgxjHXdFMP/6cj2bGIZCYEJeWwSIhz\nMbhbgK0rSgZjj2rSYNIAvRe4sQJCT7/batsRisSZtm5VAeckEdqihE8L75AvnVFl\ngtF5rTnJAgMBAAECggEACRRy7q5ge7/vq8o6HESvoqOyc9bp0tS32l4tfcwuXnmL\nwpaPFf0wHMDVlpT40a6biuY6hdX4mt0/Oo5ahmtXS2QhpwaCfC7CuKD4637W8jv2\n3NKuEeg9+rB5SZf+CGXddcmL216jBgUgqurvZsjheBi4aVdwNcv3IazKVl3Y4r/B\nX+Of47A7pGLvye9uB2dVTg1/ew1YRDratQ5JBwreBLP1YHthKgS0N72dDp46auY1\nL4kNgZhlhws0dwwqs7Uz8gk5lXMc6Dhl37SQjv1MpP45KIEvpwGLkICFDuYG8yWx\nMRniZEkacZOkUm0aBKx3uoz+tfAJYmeCAaaYy8yNAQKBgQDTEycg9TeZag7mZJLl\nbADA+nC6qFhhnUlc5t71eEn0S1ELUzm6iidwXKzqsoLH9j0IVYlKCSmW2OWdGpGy\nXjvR2GQoZd07m25ei4BCIrEKmMSVHFLcvkKAxc+GJaUV7zd5cci2FWbWcfXInOA5\nZAiAKlFd47cvdOXXCnG1IYg8gQKBgQDAMLKjaCQKyqD4bVSoChLT/A0ALHNDIqh6\niQGI9U7UaiNElTT6cOCJGwPe2N6jw84ksk2t3Mcv4mdo0qLbZ1wCinFrYQ18Bq+V\nl2z/+aj9aFlteq6uEmaS8awaZk2q5wRHG8J1R2+RU/BOUbFEpTqqNijv5omU+Dbp\nwPVv07t5SQKBgQC+eak017Udz5S0movpVwZzH019VA+1vx+GI52OPMfYGeN/6dHN\noYCnqCou/XqBgpdfHvlug0gxpXfHx0M1iE5JNxJjVlFHyiLWVOMgS3gijOvRd6bb\nJyKSXG/CqdQJMD+Ka4Dpt/R+joZzTAYYJEp7hjS5Gpajz8TdwsIL57+ugQKBgQCF\ncIpnuIPl1kCjnE1+Cth9xPBF4Zb50xOFWKDvNdtQ0ozALpTFGiBcMZyjWpJixC3Z\n/s6+W58VGS5RcoZOrdoNjZAoTQ9uwLmitKuiovZ5U71brSWhiPcdKZ0kC0n70kB/\nGsAOa9YD4nLj/gaFse8khUwxHTEDdL+z1xzSUwFEuQKBgQCmu0Md1gaxzAOfyD3s\nnGUjnUZCg2uEO7AQiCFN0VH82E+h/EKbA/qT6gGTnlsYU1U1sUSDfcLEm9XSiffR\niY2rSvapNT8yK5ndYXXNAHBK9LwHxtdtyTtjbr/c4i5gvQJDM3zbKTZKli8KznmG\nRiLaZWiIuOi+NIGxp12oRhQV8A==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-mxqf0@ramen-nado-86f76.iam.gserviceaccount.com",
-  "client_id": "102689171191973104130",
+  "project_id": "uniqueco-33e4c",
+  "private_key_id": "3727d173995971febf9bad66cc8a019ede627f10",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDsbYcUzgSHWuL6\naM5t6AsfCaEpdqmuHZyU2FKwGuBmz7KWvHHm23R2YX2Z6gElAvciMwmGy9kOT1KD\nvNxzqGBurSrAd7T05pZn/48JvZXKqoxHnrRFfnU4vYFWdekIuXRu6sU2/FXi4ubV\nmVdjEJJDJE7V1Qm1OrOdENaGCU6MDbbsX/b4XScV94FebuwU5gZrP4ZbOUS/9w6y\n3hIUjHf78nFcwqCix2XuQBDvPpZu8EnaGmPgWKLhFj8yHQMhuldo4phrv4nnURi6\n47+evUkp+qLPnM5deex47SCGybJPvLKcjEAIFhUTY0jO6xpR5hkoJqOliqzuQf3M\njEfa34OhAgMBAAECggEABAKBiX1K/16vvc58PcnzMlYP+SuNiinvZ2jZ0inKh4Pb\nRAozJlavfXh+0FbzKQUJWvehoDsh1cfLnvdbh8yhwg7GzFfbZlHo2B+x9djUywik\nc2yCIzGXXFx+bpB2YNMAYgcyTBHwhPYTMyk0HbaAvInHmoHP9dZmiHHqKYDL570l\ndJnYeHkQ1gN3kGNH2G9wOAP1BC6GKiOdQHdV/W0xBzWXnyihpJRmQUqr+2ejxPER\n0avKlt2hWbkh8WiosKPQVFNsYbQqVhRn3GpmBwaGw5JPExXGnhk/1+kSUKOn6OX8\n+gJ7mTt2AdHFEIEVFjl9WfIIwqUEK7U2h2JK+gFNqQKBgQD/1tlTuSYa9yJ0yT06\nWXDTr2Szu4DvTzFD1+AZh+ahIjcrDBEuq6QK8qUX36TNfyMvOIyaQXU72uODCi/H\n0FMlYa81NH2U3BFAAaOqmxOMofnllBtosCd7SNFIxxNf1ycTlgzIt1MepxL7gLq4\n219otKhokfr8QfmB0VOkJgbWLQKBgQDsk45zuk/3Qh3P7VO6Ucp++uv5Zpdu9+fn\noHiCy7apuxJCV4tffcACufsu8CJS/pQntC8uduCWu+yQgDsOkTNhcRblM61G3bSh\n1WeYUFjT4tjcPB2fSSnrVeaWSiQO3BsevOuTeuV0ktQSR9JGNvC73e1P/oncCVvk\nSy7iDepfxQKBgEoUy29r4HXUc/y/POSFe59AXXeR5t7k3o4Xl4OtD4I/Jxxm7R7H\ngsPMyTNlhhIfK3AD/uq345uGYXTTYUyJrVnYtGRCo86T/sa68sp3By0kxfjNbzZc\nM8KGLlvVkW2iHmWUgHUqaH4qwNtkxiy7ESB/l3hYNQYQkJAfrgmNHVOtAoGBANH0\nOi42oRU81hxb8Tyfreh3Y8jY5XgUBvmnjov1osyLOy8pZoV+olNJHsSPVMb3LCD4\nZg8EbVkYul+bjc3lywWlSb5r2FHWHKKrM98XH14cKn151IgydENo3tVuQwX1DRSA\ncUlXfh+w1wjKOLEbvRXdZOjjaxGeNLEskBUKaIBJAoGAH3NAeAkyj+BpCHVnxJsZ\np7UqLSAfL8RZCC1ltggAMjXB0CcFOVhVqVNtvvLFxvI1tJGYQfe/1PShRyw201Y7\n2HnZ9qKEuW+VXIfYlIfvuTZIIgCEmsdwkX3nOkIHBMheICMpOOS9MOVgfmx7PYM7\nMFbuDD1hDx5GiJaYJyY1cBA=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-ppfbc@uniqueco-33e4c.iam.gserviceaccount.com",
+  "client_id": "118353167285977030781",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mxqf0%40ramen-nado-86f76.iam.gserviceaccount.com",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ppfbc%40uniqueco-33e4c.iam.gserviceaccount.com"
 }
+
 )
 firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://ramen-nado-86f76-default-rtdb.asia-southeast1.firebasedatabase.app"
+    'databaseURL': "https://uniqueco-33e4c-default-rtdb.asia-southeast1.firebasedatabase.app"
 })
 
 def get_all_users():
